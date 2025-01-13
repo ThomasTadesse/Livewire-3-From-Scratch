e300367f2fc59039897246fb7d11022355af21dd
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-13 16:26:28 +0100

    introduction complete

--- livewire-from-scratch/app/Livewire/Greeter.php
+++ livewire-from-scratch/app/Livewire/Greeter.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Greeter extends Component
 {
+
+    // make a public property such as name, in this case "World"
+    // and add it later to the view
+    public $name = 'World';
+
+    // keep in mind, if it is private you will encounter an error
+
     public function render()
     {
         return view('livewire.greeter');
--- livewire-from-scratch/resources/views/livewire/greeter.blade.php
+++ livewire-from-scratch/resources/views/livewire/greeter.blade.php
@@ -1,3 +1,4 @@
 <div>
-    Hello, Livewire
+    <!-- here we place the name from the Greeter.php -->
+    Hello, {{ $name }}!
 </div>
