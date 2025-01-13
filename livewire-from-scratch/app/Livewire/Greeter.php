92658fb41779a3fae0a2e86ce41c55b9e43a5ab9
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-13 16:46:47 +0100

    update changeName method to accept dynamic name input

--- livewire-from-scratch/app/Livewire/Greeter.php
+++ livewire-from-scratch/app/Livewire/Greeter.php
@@ -14,9 +14,9 @@ class Greeter extends Component
     // keep in mind, if it is private you will encounter an error
 
     // lets make an action to change the name
-    public function changeName()
+    public function changeName($newName)
     {
-        $this->name = "John";
+        $this->name = $newName;
     }
 
 
--- livewire-from-scratch/resources/views/livewire/greeter.blade.php
+++ livewire-from-scratch/resources/views/livewire/greeter.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="mt-2">
         <button class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
-                wire:click="changeName()"
+                wire:click="changeName('John')"
         >
             Click me
         </button>
