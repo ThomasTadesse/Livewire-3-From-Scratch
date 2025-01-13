703b2f5edc079518f32541ea2773ae725dcf6f04
ThomasTadesse <328704@student.mboutrecht.nl>
2025-01-13 17:05:42 +0100

    add form for dynamic name submission

--- livewire-from-scratch/resources/views/livewire/greeter.blade.php
+++ livewire-from-scratch/resources/views/livewire/greeter.blade.php
@@ -3,12 +3,18 @@
         <!-- here we place the name from the Greeter.php -->
     Hello, {{ $name }}!
     </div>
+    <form 
+        wire:submit="changeName(document.getElementById('newName').value)"
+
+    
+    >
     <div class="mt-2">
         <!-- enter name in changeName() to change the name to that value on click -->
         <input 
         id = "newName"
         type="text" 
         class="block w-full p-4 border rounded-md bg-gray-700 text-white"
+        placeholder="Enter new name"
         
     >
      
@@ -17,10 +23,11 @@
 
     <div class="mt-2">
         <!-- enter name in changeName() to change the name to that value on click -->
-        <button class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
-                wire:click="changeName(document.getElementById('newName').value)"
+        <button type="submit"
+                class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
         >
             Click me
         </button>
     </div>
+    </form>
 </div>
